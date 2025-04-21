1ceb65c707ff7b18e79a458a9138e25ec1181d1e
fake js dev <112frosta@proton.me>
2025-04-21 15:03:53 +0200

    Refactor types.d.ts to organize command and feature type declarations

--- types.d.ts
+++ types.d.ts
@@ -2,7 +2,10 @@ import type { CommandBuilder } from "@/lib/CommandBuilder";
 import type { CommandInteraction } from "discord.js";
 
 declare global {
+  // Commands
   type Interaction = CommandInteraction<"cached">;
   type CommandConfig = CommandBuilder;
   type CommandExecute = (interaction: Interaction) => Promise<unknown>;
+
+  // Features
 }
