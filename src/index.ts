d98bab9fb0867ed0ab9a97abdd95c238208a004b
fake js dev <112frosta@proton.me>
2025-04-21 13:32:12 +0200

    Init Commit

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import { Client } from "discord.js";
+
+export const client = new Client({
+  intents: ["Guilds", "GuildMessages", "GuildMembers", "MessageContent"],
+});
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user?.tag}`);
+});
+
+client.login(process.env.BOT_TOKEN);
