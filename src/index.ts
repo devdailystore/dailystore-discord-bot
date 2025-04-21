7c34a31522f8f56575068bcce90976f54558d7f0
fake js dev <112frosta@proton.me>
2025-04-21 13:45:04 +0200

    Replace console.log with Console.Log for consistent logging

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import { Client } from "discord.js";
+import { Console } from "@lib/Console";
 
 export const client = new Client({
   intents: ["Guilds", "GuildMessages", "GuildMembers", "MessageContent"],
 });
 
 client.on("ready", () => {
-  console.log(`Logged in as ${client.user?.tag}`);
+  Console.Log(`Logged in as ${client.user?.tag}`);
 });
 
 client.login(process.env.BOT_TOKEN);
