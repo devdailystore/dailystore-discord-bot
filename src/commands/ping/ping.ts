0c1a9442f334446744c15a4b5eceb9bef28033b3
fake js dev <112frosta@proton.me>
2025-04-21 14:34:17 +0200

    Add ping command with configuration and execution logic

---
+++ src/commands/ping/ping.ts
@@ -0,0 +1,12 @@
+import { CommandBuilder } from "@/lib/CommandBuilder";
+
+export const config: CommandConfig = new CommandBuilder()
+  .setName("ping")
+  .setDescription("Ping the bot")
+  .setDisabled(true);
+
+export const execute: CommandExecute = async (interaction) => {
+  return await interaction.reply({
+    content: "Pong!",
+  });
+};
