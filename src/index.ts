e914d5402629cd9d45602c9229f8387b553c9121
fake js dev <112frosta@proton.me>
2025-06-03 16:02:06 +0200

    Add GuildMembers intent to Discord client configuration

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { Client } from "discord.js";
 
 export const client = new Client({
-  intents: ["Guilds", "GuildMessages", "MessageContent"],
+  intents: ["Guilds", "GuildMessages", "GuildMembers", "MessageContent"],
 });
 
 await client.login(process.env.BOT_TOKEN);
